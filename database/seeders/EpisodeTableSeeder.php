fe44f4cdd21205d8d7fe9a63fd0f87d4b5f46330
Matthew Horrocks <mlhorrocks01@gmail.com>
2021-06-03 11:46:34 +0100

    Change EpisodeSeeder to use Episode's factory

--- database/seeders/EpisodeTableSeeder.php
+++ database/seeders/EpisodeTableSeeder.php
@@ -15,17 +15,6 @@ class EpisodeTableSeeder extends Seeder
     public function run()
     {
         Episode::truncate();
-
-        $faker = \Faker\Factory::create();
-        // Generate some fake data with faker
-        for ($i=0; $i < 20; $i++) { 
-            Episode::create([
-                'download_url' => $faker->url,
-                'title' => $faker->sentence,
-                'description' => $faker->paragraph,
-                'episode_number' => $faker->randomNumber($nbDigits = 4)
-            ]);
-        }
-        //
+        Episode::factory()->count(20)->create();
     }
 }
